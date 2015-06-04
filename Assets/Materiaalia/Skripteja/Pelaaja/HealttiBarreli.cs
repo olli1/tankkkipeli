09294e9d95b44cd98c6d10ce31234d46e9b5ed73
Olli Isohanni <ollih2@gmail.com>
2015-06-04 14:58:38 +0300

    hdfj

---
+++ Assets/Materiaalia/Skripteja/Pelaaja/HealttiBarreli.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealttiBarreli : MonoBehaviour {
+
+	float barrellHealth = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		if(barrellHealth <= 0){ //aktivoi tuhoutumisen
+			
+			Die ();
+		}
+
+
+
+	
+	}
+
+
+
+
+
+
+
+
+	void OnTriggerEnter2D()
+		
+	{
+		Debug.Log ("triggeri");//testi
+		
+
+
+		barrellHealth--;//törmäyksessä vähentää health pisteen
+
+		
+		
+		
+		
+		
+	}
+
+
+
+
+
+
+	void Die()//tuhoutuminen
+	{
+		Destroy (gameObject);
+		
+	}
+
+
+
+
+
+
+
+}
